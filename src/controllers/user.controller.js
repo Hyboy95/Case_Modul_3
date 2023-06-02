09de9533823120fabaeb4ba4e98e233402180a53
Phong <ngphong.0708@gmail.com>
2023-06-02 10:52:39 +0700

    phong 10:52

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,57 +1,53 @@
 const qs = require('qs');
-const url = require('url');
+const url = require("url");
 
 const BaseController = require('./base.controller');
+const GeneralController = require('./../controllers/general.controller');
+const ProductController = require('./../controllers/product.controller');
 const productModel = require('./../models/product.model');
-class ProductController {
-    static async getListProduct(page, func) {
-        let products = await func;
-        let startIndex = (parseInt(page) - 1) * 12;
-        let endIndex = page * 12;
-        let paginatedProducts = products.slice(startIndex, endIndex);
-        let newHtml = '';
-        paginatedProducts.forEach((product) => {
-            newHtml += `<div class="col-md-3 mb-5">`;
-            newHtml += `<div class="card h-100">`;
-            newHtml += `<img class="card-img-top" src="${product.pImg}" alt="...">`;
-            newHtml += `<div class="card-body p-4">`;
-            newHtml += `<div class="text-center">`;
-            newHtml += `<p class="fw-bolder">${product.pName}</p>`;
-            newHtml += `<p class="fw-bolder">Size: ${product.pSize}cm</p>`;
-            newHtml += `<p class="fw-bolder">${parseInt(product.pPrice).toLocaleString()} VNĐ</p>`;
-            newHtml += `</div>
-                        </div>
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/login">Thêm vào giỏ hàng</a></div>
-                        </div>
-                    </div>
-                </div>`
-        })
-        let totalPages = Math.ceil(products.length / 12);
-        return {
-            listProducts: newHtml,
-            numPage: totalPages
+
+class UserController {
+    static async handlerUserHomePage(req, res) {
+        if (req.method === "GET") {
+            let getAllProducts = productModel.getAllProduct();
+            await ProductController.getBasePage(req, res, getAllProducts, '?page=', './src/views/user/UserHomePage.html')
+        } else {
+            let searchValue = await GeneralController.getDataByForm(req, res);
+            let getSearchProduct = productModel.getSearchProduct(searchValue.search);
+            await ProductController.getBasePage(req, res, getSearchProduct, '?page=', './src/views/user/UserHomePage.html');
         }
     }
-    static async getBasePage(req, res, func, link, filePath) {
+
+    static async handlerFilterUserHomePage(req, res) {
         let query = qs.parse(url.parse(req.url).query);
-        let products = await func;
-        let html = await BaseController.readFileData(filePath);
-        let htmlReplace = await ProductController.getListProduct(query.page ? query.page : 1, products);
-        let newHtml = htmlReplace.listProducts;
-        let totalPages = htmlReplace.numPage;
-        let paginationHtml = '';
-        for (let i = 1; i <= totalPages; i++) {
-            paginationHtml += `<li class="page-item"></li>`;
-            paginationHtml += `<a class="page-link" href="${link}${i}">${i}</a>`;
-            paginationHtml += `</li>`;
+        if (query.type && req.method === 'GET') {
+            let type = qs.parse(url.parse(req.url).query).type;
+            let getProductFilterByType = productModel.getProductByType(type);
+            await ProductController.getBasePage(req, res, getProductFilterByType, `user/filter?type=${type}&page=`, './src/views/user/UserHomePage.html');
+        } else if (query.minprice && req.method === 'GET') {
+            let minPrice = qs.parse(url.parse(req.url).query).minprice;
+            let maxPrice = qs.parse(url.parse(req.url).query).maxprice;
+            let getProductFilterByPrice = productModel.getProductByPrice(minPrice, maxPrice);
+            await ProductController.getBasePage(req, res, getProductFilterByPrice, `user/filter?minprice=${minPrice}&maxprice=${maxPrice}&page=`, './src/views/user/UserHomePage.html');
+        } else if (query.minsize && req.method === 'GET') {
+            let minSize = qs.parse(url.parse(req.url).query).minsize;
+            let maxSize = qs.parse(url.parse(req.url).query).maxsize;
+            let getProductFilterBySize = productModel.getProductBySize(minSize, maxSize);
+            await ProductController.getBasePage(req, res, getProductFilterBySize, `user/filter?minsize=${minSize}&maxsize=${maxSize}&page=`, './src/views/user/UserHomePage.html');
         }
-        html = html.replace('{list-product}', newHtml);
-        html = html.replace('{pagin}', paginationHtml);
-        res.writeHead(200, {'Content-type': 'text/html'});
-        res.write(html);
-        res.end();
+    }
+    static async handlerUserProfilePage(req, res) {
+        if (req.method === "GET") {
+            let html = await BaseController.readFileData('./src/views/user/users-profile.html');
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(html);
+            res.end();
+        } else {
+            let searchValue = await GeneralController.getDataByForm(req, res);
+            let getSearchProduct = productModel.getSearchProduct(searchValue.search);
+            await ProductController.getBasePage(req, res, getSearchProduct, '?page=', './src/views/user/UserHomePage.html');
+        }
     }
 }
 
-module.exports = ProductController;
+module.exports = UserController;

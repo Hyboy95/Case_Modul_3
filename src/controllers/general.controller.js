99f18fc2c383bf7ae69f807e95e8705ee4b179f8
Hyboy95 <temilator@gmail.com>
2023-06-02 14:35:59 +0700

    admin_view

---
+++ src/controllers/general.controller.js
@@ -0,0 +1,82 @@
+const qs = require("qs");
+
+const BaseController = require('./base.controller');
+const generalModel = require('../models/general.model');
+
+class GeneralController {
+
+    static async getDataByForm(req, res) {
+        const buffers = [];
+        for await (const chunk of req) {
+            buffers.push(chunk);
+        }
+        const data = Buffer.concat(buffers).toString();
+        return qs.parse(data);
+    }
+
+    static async handlerLoginPage(req, res) {
+        if (req.method === "GET") {
+            let html = await BaseController.readFileData('./src/views/General/Login.html');
+            res.writeHead(200, { 'Content-type': 'text/html' });
+            res.write(html);
+            res.end();
+        } else {
+            let user = await GeneralController.getDataByForm(req, res);
+            let { username, password } = user;
+            let infoUser = await generalModel.login(username, password);
+            if (infoUser !== 0) {
+                let roleBuffer = infoUser.role;
+                let role = roleBuffer.readUInt8(0);
+                if (role === 1) {
+                    console.log('Login success with role admin!');
+                    res.writeHead(302, { location: '/admin' });
+                    res.end();
+                } else if (role === 0) {
+                    console.log('Login success with role user!');
+                    await BaseController.writeFileData('./session/dataUser.json', JSON.stringify(infoUser)).catch((err) => {
+                        console.log(err.message)
+                    });
+                    res.writeHead(302, { location: `/user` });
+                    res.end();
+                }
+            } else {
+                console.log('Login fail!');
+                res.writeHead(301, { location: '/login' });
+                res.end();
+            }
+        }
+    }
+
+    static async handlerRegister(req, res) {
+        if (req.method === "GET") {
+            let html = await BaseController.readFileData('./src/views/General/Register.html');
+            res.writeHead(200, { 'Content-type': 'text/html' });
+            res.write(html);
+            res.end();
+        } else {
+            let data = await GeneralController.getDataByForm(req, res);
+            let { name, email, phone, address, username, password } = data;
+
+            let usernameExists = await generalModel.checkExistsAccount(username, email);
+            if (!usernameExists) {
+                await generalModel.registerAccount(username, password, name, phone, email, address);
+                console.log('Register success!');
+                res.writeHead(301, { location: '/login' });
+                res.end();
+            } else {
+                console.log('Account was exists');
+                res.writeHead(301, { location: '/register' });
+                res.end();
+            }
+        }
+    }
+
+    static async getNotFoundPage(req, res) {
+        let html = await BaseController.readFileData('./src/views/General/NotFound.html');
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.write(html);
+        res.end();
+    }
+}
+
+module.exports = GeneralController;
---
+++ src/controllers/product.controller.js
@@ -0,0 +1,67 @@
+const qs = require('qs');
+const url = require('url');
+
+const BaseController = require('./base.controller');
+const productModel = require('./../models/product.model');
+
+class ProductController {
+    static async getListProduct(page, func) {
+        let products = await func;
+        let startIndex = (parseInt(page) - 1) * 12;
+        let endIndex = page * 12;
+        let paginatedProducts = products.slice(startIndex, endIndex);
+        let newHtml = '';
+        paginatedProducts.forEach((product) => {
+            newHtml += `<div class="col-md-3 mb-5">`;
+            newHtml += `<div class="card h-100">`;
+            newHtml += `<img class="card-img-top" src="${product.pImg}" alt="...">`;
+            newHtml += `<div class="card-body p-4">`;
+            newHtml += `<div class="text-center">`;
+            newHtml += `<p class="fw-bolder">${product.pName}</p>`;
+            newHtml += `<p class="fw-bolder">Size: ${product.pSize}cm</p>`;
+            newHtml += `<p class="fw-bolder">${parseInt(product.pPrice).toLocaleString()} VNĐ</p>`;
+            newHtml += `</div>
+                        </div>
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/login">Thêm vào giỏ hàng</a></div>
+                        </div>
+                    </div>
+                </div>`
+        })
+        let totalPages = Math.ceil(products.length / 12);
+        return {
+            listProducts: newHtml,
+            numPage: totalPages
+        }
+    }
+
+    static async getBasePage(req, res, func, link, filePath) {
+        let query = qs.parse(url.parse(req.url).query);
+        let products = await func;
+        let html = await BaseController.readFileData(filePath);
+        let htmlReplace = await ProductController.getListProduct(query.page ? query.page : 1, products);
+        let newHtml = htmlReplace.listProducts;
+        let totalPages = htmlReplace.numPage;
+        let paginationHtml = '';
+        for (let i = 1; i <= totalPages; i++) {
+            paginationHtml += `<li class="page-item"></li>`;
+            paginationHtml += `<a class="page-link" href="${link}${i}">${i}</a>`;
+            paginationHtml += `</li>`;
+        }
+        html = html.replace('{list-product}', newHtml);
+        html = html.replace('{pagin}', paginationHtml);
+        if (filePath === './src/views/user/UserHomePage.html') {
+            let dataUser = await BaseController.readFileData('./session/dataUser.json').catch(err => {
+                console.log(err.message);
+            });
+            let name = JSON.parse(dataUser).name;
+            html = html.replace('{Username1}', name);
+            html = html.replace('{Username2}', name);
+        }
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.write(html);
+        res.end();
+    }
+}
+
+module.exports = ProductController;
